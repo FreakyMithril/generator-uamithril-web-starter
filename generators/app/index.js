858c9692f2bf075d8907922caafe5071dbb3a0bf
Alex Trofymenko <mithrilwork@gmail.com>
2016-10-14 23:52:49 +0300

    start add promts

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,33 +4,52 @@ var chalk = require('chalk');
 var yosay = require('yosay');
 
 module.exports = yeoman.Base.extend({
-  prompting: function () {
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the ' + chalk.red('uamithril-web-starter') + ' generator!'
-    ));
+	prompting: function () {
+		this.log(yosay(
+			'Welcome to the ' + chalk.red('uamithril-web-starter') + ' generator!'
+		));
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this Test option?(Nothing happened - it is just a test)',
-      default: true
-    }];
+		var prompts = [
+			{
+				type: 'input',
+				name: 'projectTitleName',
+				message: 'Type Title name of your project (Will be included in many places like title)',
+				default: 'New project'
+			},
+			{
+				type: 'input',
+				name: 'projectName',
+				message: 'Type short name of your project (Lowercase and with NO spaces)',
+				default: 'new-project'
+			},
+			{
+				type: 'confirm',
+				name: 'someAnswer',
+				message: 'Would you like to enable this Test option? (Nothing happened - it is just a test)',
+				default: true
+			},
+			{
+				type: 'confirm',
+				name: 'useBuildInGit',
+				message: 'Do you agree to NOT include "build" folder of project inside GIT repo? (it will make simpler commits)',
+				default: true
+			}
+		];
 
-    return this.prompt(prompts).then(function (props) {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    }.bind(this));
-  },
+		return this.prompt(prompts).then(function (props) {
+			// To access props later use this.props.someAnswer;
+			this.props = props;
+		}.bind(this));
+	},
 
-  writing: function () {
-    this.fs.copy(
-      this.templatePath(),
-      this.destinationPath()
-    );
-  },
+	writing: function () {
+		this.fs.copy(
+			this.templatePath(),
+			this.destinationPath()
+		);
+	},
 
-  install: function () {
-    this.npmInstall();
-  }
+	install: function () {
+		this.npmInstall();
+	}
 });
